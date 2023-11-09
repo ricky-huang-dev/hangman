db0922ea1cd6b310696430cbd0861200895766c3
Cong Guo <jeraviah@gmail.com>
2023-11-09 22:54:52 +1300

    Init Typing in letters

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -3,37 +3,46 @@ import { getGreeting } from '../apiClient.ts'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-  const [isError, setIsError] = useState(false)
+  const [targetWord, setTargetWord] = useState([] as string[])
+  const [apiWord, setApiWord] = useState('')
 
+  const lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  
+  
   useEffect(() => {
-    async function updateGreeting() {
-      try {
-        const greeting = await getGreeting()
-        setGreeting(greeting)
-        setIsError(false)
-      } catch (err) {
-        setIsError(true)
-      }
+    const answerWord = 'HELLO'
+
+    const myArr = []
+    for (let i = 0; i < answerWord.length; i++) {
+      myArr.push('_')
     }
 
-    updateGreeting()
-  }, [count])
+    setTargetWord(myArr)
+    setApiWord(answerWord)
+  },[])
+
+  function handleClick(e: React.MouseEvent<HTMLElement>) {
+    const apiWordArr = apiWord.split('')
+    const letterTyped = e.target.innerHTML
+    const newArr = apiWordArr.map((e,index) => {
+      if (targetWord[index]!=='_') return targetWord[index]
+      if (e === letterTyped) return e
+      else return '_'
+    })
+    setTargetWord(newArr)
+    if (targetWord.join('') === apiWord) {
+      alert('You win!')
+    }
+  }
 
   return (
     <>
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-      <p>
-        <Link to="/frogs/kevin">Is there a frog named Kevin?</Link>
-      </p>
+      {targetWord.map((e, index) => (
+          <p style={{display:'inline-block', margin:'8px'}} key={index}>{e}</p>
+        ))}<br/>
+      {lettersArr.map((letter,index) => (
+          <button name={letter} onClick={handleClick} key={index}>{letter}</button>
+        ))}
     </>
   )
 }
